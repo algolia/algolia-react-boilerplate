3257f956e109ce320c0e318a45e25f171db01ac4
Alexandre Viretti <alexou73@gmail.com>
2022-01-31 17:18:22 +0100

    fix: create a redirects file to fix it in netlify

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, "public", "index.html"),
     }),
   ],
 };
