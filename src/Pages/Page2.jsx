6df36be1b30c3e21db753a2dc74b55a23394b277
Alexandre Viretti <alexou73@gmail.com>
2022-01-31 16:27:33 +0100

    feat: integrate optionnal chaining into babel with a gonfig into babelrc, then you can test on page2

--- src/Pages/Page2.jsx
+++ src/Pages/Page2.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+// Recoil
+import { atom, useRecoilState, selector, useRecoilValue } from "recoil";
+
 const Page2 = () => {
-  return <h1>Page2</h1>;
+  const [optionnal, setOptionnal] = useRecoilState(optionnalTest);
+  const thirdLevelValue = optionnal?.firstLevel?.secondLevel?.thirdLevel;
+  return (
+    <div>
+      <p>Page 2 // {thirdLevelValue}</p>
+    </div>
+  );
 };
 
 export default Page2;
+
+const optionnalTest = atom({
+  key: "optionnalTest", // unique ID (with respect to other atoms/selectors)
+  default: {
+    firstLevel: {
+      secondLevel: {
+        thirdLevel: "value",
+      },
+    },
+  }, // default value (aka initial value)
+});
