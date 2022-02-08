a7652d99c6bb35373b9bf798a91b168581c42d5d
Alexandre Viretti <alexou73@gmail.com>
2022-02-08 12:39:59 +0100

    feat: init desing on button on banner 2 with circles

---
+++ src/components/Banners.jsx/HomeBanner2.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HomeBanner2 = () => {
+  return (
+    <div className="home-banner2-container">
+      <div className="home-banner2-container__infos">
+        <h1>TITLE</h1>
+        <h2>Subtitle</h2>
+      </div>
+      <div className="home-banner2-container__buttons">
+        <div className="home-banner2-container__buttons__circles">
+          <div className="home-banner2-container__buttons__circles__circles2">
+            <div className="home-banner2-container__buttons__circles__circles2__circles3">
+              <a href="#">EXPLORE</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner2;
