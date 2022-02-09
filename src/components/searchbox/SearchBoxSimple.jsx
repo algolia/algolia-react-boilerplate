e9614ec88a5a4ae1ac5ba793da80f412562bcfd2
Alexandre Viretti <alexou73@gmail.com>
2022-02-09 09:39:37 +0100

    fix: svg import as component

--- src/components/searchbox/SearchBoxSimple.jsx
+++ src/components/searchbox/SearchBoxSimple.jsx
@@ -1,13 +1,16 @@
 // This SearchBox is with a glass inside
 // but simple
 
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 // Import Recoil
 // &&
 // application state from config file
-import { queryAtom, simplePlaceholderAtom } from "../../config/searchbox";
-import { useRecoilState } from "recoil";
+import { queryAtom, simplePlaceholderAtom } from '../../config/searchbox';
+import { useRecoilState } from 'recoil';
+
+// Import SVG
+import Glass from '../../assets/svg/Glass';
 
 export default function SearchBoxSimple() {
   // State for the SearchBox
@@ -36,20 +39,7 @@ export default function SearchBoxSimple() {
           }}
           placeholder={simplePlaceholder}
         />
-        <svg
-          viewBox="0 0 897 897"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M357.982 142.492C236.887 142.492 138.404 241.174 138.404 362.265C138.404 483.356 236.887 581.843 357.982 581.843C479.077 581.843 577.755 483.356 577.755 362.265C577.755 241.174 479.077 142.492 357.982 142.492ZM357.982 182.008C457.701 182.008 538.239 262.551 538.239 362.265C538.239 461.977 457.701 542.327 357.982 542.327C258.264 542.327 177.921 461.977 177.921 362.265C177.921 262.551 258.264 182.008 357.982 182.008Z"
-            fill="#030A2B"
-          />
-          <path
-            d="M501.254 479.455C497.515 479.851 493.966 481.307 491.024 483.649C488.082 485.99 485.868 489.122 484.643 492.676C483.417 496.231 483.23 500.06 484.103 503.717C484.976 507.375 486.874 510.708 489.574 513.326L671.582 695.334C673.386 697.323 675.575 698.924 678.017 700.039C680.459 701.155 683.103 701.764 685.787 701.825C688.471 701.886 691.139 701.399 693.63 700.395C696.12 699.392 698.38 697.893 700.271 695.988C702.163 694.083 703.647 691.812 704.634 689.315C705.62 686.818 706.087 684.145 706.007 681.461C705.928 678.778 705.302 676.14 704.17 673.705C703.037 671.271 701.421 669.092 699.419 667.303L517.411 485.294C515.33 483.169 512.797 481.539 509.999 480.528C507.202 479.517 504.213 479.151 501.254 479.455Z"
-            fill="#030A2B"
-          />
-        </svg>
+        <Glass />
       </form>
     </div>
   );
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 // React router
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import { atom, useRecoilState, selector, useRecoilValue } from "recoil";
+import { atom, useRecoilState, selector, useRecoilValue } from 'recoil';
 
 // application state from config file
-import { configAtom } from "../config/config";
+import { configAtom } from '../config/config';
 
 const Test = () => {
   // access config state and log for testing
@@ -29,14 +29,14 @@ export default Test;
 // An atom represents a piece of state. Atoms can be read from and written to from any component.
 // Components that read the value of an atom are implicitly subscribed to that atom, so any atom updates will result in a re-render of all components subscribed to that atom:
 const textState = atom({
-  key: "textState", // unique ID (with respect to other atoms/selectors)
-  default: "", // default value (aka initial value)
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: '', // default value (aka initial value)
 });
 
 // A selector represents a piece of derived state. Derived state is a transformation of state.
 // You can think of derived state as the output of passing state to a pure function that modifies the given state in some way:
 const charCountState = selector({
-  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
   get: ({ get }) => {
     const text = get(textState);
     return text.length;
@@ -67,6 +67,6 @@ function CharacterCount() {
 
 function BadFunction() {
   useEffect(() => {
-    console.log("bad");
+    console.log('bad');
   });
 }
