2219562801b1743099bed92a4b23d8a18eb7c0ca
Matt Foyle <mfoyle93@gmail.com>
2022-02-10 08:57:47 -0600

    wip: results page

---
+++ src/pages/ResultsPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// Recoil
+import { useRecoilState } from 'recoil';
+import { hitsAtom } from '../config/results';
+
+import { Hits } from 'react-instantsearch-dom';
+
+const ResultsPage = () => {
+  const [hitsState] = useRecoilState(hitsAtom);
+
+  console.log(hitsState)
+  return (
+    <div>
+      <p>Results Page // {hitsState}</p>
+      <Hits />
+    </div>
+  );
+};
+
+export default ResultsPage;
