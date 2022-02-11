30f79e1471e29b2533520495b244be0848417e0b
Alexandre Viretti <alexou73@gmail.com>
2022-02-11 15:26:03 +0100

    fix: class on injected hits

--- src/components/searchresultpage/injected-hits/InjectedInfiniteHits.jsx
+++ src/components/searchresultpage/injected-hits/InjectedInfiniteHits.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimateSharedLayout } from 'framer-motion';
 import React from 'react';
 import { createClassNames, connectInfiniteHits } from 'react-instantsearch-dom';
 import { connectInjectedHits } from './connectInjectedHits';
@@ -6,20 +7,20 @@ const cx = createClassNames('InfiniteHits');
 
 export const InjectedInfiniteHits = connectInfiniteHits(
   connectInjectedHits(({ injectedHits, hasMore, refineNext }) => (
-    <div className={cx('')}>
-      <ul className={cx('list')}>
-        {injectedHits.map(({ props, type, Hit }, index) => {
-          return (  
-              <Hit {...props} index={index} key={index} />
-          );
-        })}
-      </ul>
-    </div>    
-    ))
-  );
-  
-  // {hasMore ? (
-  //   <button className={cx('loadMore')} onClick={refineNext}>
-  //     Load more
-  //   </button>
-  // ) : null}
+    <AnimateSharedLayout>
+      <motion.div layout className={cx('')}>
+        <motion.ul layout className={cx('list')}>
+          {injectedHits.map(({ props, type, Hit }, index) => {
+            return <Hit {...props} index={index} key={index} />;
+          })}
+        </motion.ul>
+      </motion.div>
+    </AnimateSharedLayout>
+  ))
+);
+
+// {hasMore ? (
+//   <button className={cx('loadMore')} onClick={refineNext}>
+//     Load more
+//   </button>
+// ) : null}
