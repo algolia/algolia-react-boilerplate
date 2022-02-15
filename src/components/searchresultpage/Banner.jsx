8ee47a787d703f0884f388a605b2974e70ca655b
Alexandre Viretti <alexou73@gmail.com>
2022-02-15 15:47:30 +0100

    fix: &&

--- src/components/searchresultpage/Banner.jsx
+++ src/components/searchresultpage/Banner.jsx
@@ -6,20 +6,20 @@ const Banner = () => {
     <QueryRuleCustomData>
       {({ items }) =>
         items.map((item) => {
-          return item.type === 'bannerSrp' ? (
-            <div
-              className="banner-srp"
-              style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.62), rgba(0, 0, 0, 0.45)), url(${item.banner})`,
-              }}
-            >
-              <div className="banner-srp__infos">
-                <h3>{item.title}</h3>
-                <p>{item.content}</p>
+          return (
+            item.type === 'bannerSrp' && (
+              <div
+                className="banner-srp"
+                style={{
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.62), rgba(0, 0, 0, 0.45)), url(${item.banner})`,
+                }}
+              >
+                <div className="banner-srp__infos">
+                  <h3>{item.title}</h3>
+                  <p>{item.content}</p>
+                </div>
               </div>
-            </div>
-          ) : (
-            ''
+            )
           );
         })
       }
