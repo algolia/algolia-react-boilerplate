68a886d6121649b761f06ef28c7cfec0e7381820
Hugo Valla <hugo_valla@hotmail.fr>
2022-02-15 14:14:18 +0100

    fix: cleaning the custom placeholder in config file

--- src/config/searchbox.js
+++ src/config/searchbox.js
@@ -5,11 +5,6 @@ export const simplePlaceholderAtom = atom({
   default: 'Search...', // default value (aka initial value)
 });
 
-export const customPlaceholderAtom = atom({
-  key: 'customplaceholderAtom', // unique ID (with respect to other atoms/selectors)
-  default: 'Search', // default value (aka initial value)
-});
-
 export const queryAtom = atom({
   key: 'queryAtom', // unique ID (with respect to other atoms/selectors)
   default: null, // default value (aka initial value)
