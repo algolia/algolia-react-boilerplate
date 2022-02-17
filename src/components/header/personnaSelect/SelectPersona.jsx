e240fb8c71cd43179f71637934208fcca86d3c38
Alexandre Viretti <alexou73@gmail.com>
2022-02-17 10:41:50 +0100

    fix: color and style font option react select

--- src/components/header/personnaSelect/SelectPersona.jsx
+++ src/components/header/personnaSelect/SelectPersona.jsx
@@ -2,8 +2,20 @@ import React, { useEffect } from 'react';
 import Select from 'react-select';
 
 import { useSetRecoilState, useRecoilValue } from 'recoil';
-
 import { personaConfig, personaSelected } from '../../../config/header';
+
+const colorStyles = {
+  placeholder: (defaultStyles) => {
+    return {
+      ...defaultStyles,
+      fontFamily: "'Raleway', sans-serif",
+    };
+  },
+  option: (provided) => ({
+    ...provided,
+    fontFamily: "'Raleway', sans-serif",
+  }),
+};
 
 const SelectPersona = () => {
   const persona = useRecoilValue(personaConfig);
@@ -14,6 +26,7 @@ const SelectPersona = () => {
       <Select
         defaultValue={personaSelect}
         options={persona}
+        styles={colorStyles}
         placeholder="Persona"
         onChange={setPersonaSelect}
       />
