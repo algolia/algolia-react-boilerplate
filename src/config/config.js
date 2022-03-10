b89b523ad92e39b77f5cd9ee9fb85cd53633c452
Alexandre Viretti <alexou73@gmail.com>
2022-03-10 14:39:29 +0100

    fix: config refinement

--- src/config/config.js
+++ src/config/config.js
@@ -133,6 +133,11 @@ const hitsPerPage = {
   numberInjected: 14,
 };
 
+const refinementPrice = {
+  moreThan: 'More than',
+  lessThan: 'Less than',
+};
+
 // ------------------------------------------
 // Carousel Config
 // ------------------------------------------
@@ -189,27 +194,31 @@ export const framerMotionHits = {
   transition: { duration: 0.5, delay: 0.3, ease: [0.43, 0.13, 0.23, 0.96] },
 };
 
-export const framerMotionTransition = { delay: 0.1, duration: 0.6,  ease: [0.43, 0.13, 0.23, 0.96] }
+export const framerMotionTransition = {
+  delay: 0.1,
+  duration: 0.6,
+  ease: [0.43, 0.13, 0.23, 0.96],
+};
 
 export const framerMotionPage = {
   initial: { opacity: 0 },
   exit: { opacity: 0 },
-  animate: { opacity: 1, },
+  animate: { opacity: 1 },
   transition: { duration: 1, ease: [0.43, 0.13, 0.23, 0.96] },
-}
+};
 
 export const framerMotionFederatedContainer = {
   initial: { y: '-100%', opacity: 0 },
-  exit: { y: "-100%", opacity: 0 },
-  animate: { y:0, opacity: 1 },
-  transition: {  duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96] },
-}
+  exit: { y: '-100%', opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  transition: { duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96] },
+};
 export const framerMotionFacet = {
   initial: { opacity: 0 },
   exit: { opacity: 0 },
-  animate: { opacity: 1, },
+  animate: { opacity: 1 },
   transition: { delay: 0.2, duration: 1, ease: [0.43, 0.13, 0.23, 0.96] },
-}
+};
 
 const config = {
   refinements,
@@ -224,6 +233,7 @@ const config = {
   currency,
   injectedHits,
   sortBy,
+  refinementPrice,
 };
 
 export const configAtom = atom({
