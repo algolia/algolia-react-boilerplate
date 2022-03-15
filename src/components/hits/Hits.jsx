effb73f7e97a20a1731cbf489a7ee31787fa5fb2
Alexandre Viretti <alexou73@gmail.com>
2022-03-15 12:01:06 +0100

    fix: key name for currency atom

--- src/components/hits/Hits.jsx
+++ src/components/hits/Hits.jsx
@@ -21,6 +21,7 @@ const Hit = ({ hit }) => {
   // Get hit attribute from config file
   const { price, objectID, image, category, productName } =
     useRecoilValue(hitsConfig);
+  console.log('image', hit[image]);
 
   return (
     <motion.li
