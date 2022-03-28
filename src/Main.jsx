1120c104b11045ff63a76c174e1e3ac139b83ebc
Hugo Valla <Hugo_valla@hotmail.fr>
2022-03-28 14:53:44 +0200

    feat: loader added when starting the app

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // Framer-Motion
 import { AnimatePresence } from 'framer-motion';
 
 // React router
 import { Routes, Route, useLocation } from 'react-router-dom';
+
+//Import custom Hook
+import useIsMounted from './hooks/useIsMounted';
 
 // Import Pages
 import Header from './components/header/Header';
@@ -12,8 +15,15 @@ import SearchResultsPage from './pages/SearchResultsPage';
 import ProductDetails from './pages/ProductDetails';
 import Footer from './components/footer/Footer';
 
-export const Main = () => {
+export const Main = ({ setIsLoaded }) => {
   const location = useLocation();
+
+  //check is component is mounted
+  const isMounted = useIsMounted();
+  if (isMounted()) {
+    setIsLoaded(true);
+  }
+
   return (
     <>
       <Header />
