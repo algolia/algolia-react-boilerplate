8a70c471738e6977c8c812981a79571e8775a3bd
Hugo Valla <Hugo_valla@hotmail.fr>
2022-03-29 16:16:17 +0200

    fix: pdp

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -50,7 +50,8 @@ const ProductDetails = () => {
   const { tablet, mobile } = useScreenSize();
 
   // Get hit attribute from config file
-  const { price, objectID, image, productName } = useRecoilValue(hitsConfig);
+  const { price, objectID, image, productName, colour, sizeFilter, brand } =
+    useRecoilValue(hitsConfig);
 
   return (
     // Product Display Page parent container, including attributes for framer motion
