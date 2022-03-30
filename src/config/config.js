e422b1696c299071a9fcff1b13f480878bf64c08
Matt Foyle <mfoyle93@gmail.com>
2022-03-29 23:04:27 -0500

    chore: isolate facets config

--- src/config/config.js
+++ src/config/config.js
@@ -63,78 +63,9 @@ export const selectSortByIndex = atom({
   default: '', // default value (aka initial value)
 });
 
-// ------------------------------------------
-// Refinements Config
-// ------------------------------------------
-
-const refinements = [
-  {
-    type: 'hierarchical',
-    header: 'Categories',
-    label: 'Category',
-    options: {
-      attribute: [
-        'hierarchicalCategories.lvl0',
-        'hierarchicalCategories.lvl1',
-        'hierarchicalCategories.lvl2',
-        'hierarchicalCategories.lvl3',
-      ],
-      searchable: true,
-    },
-  },
-  {
-    type: 'price',
-    header: 'Price',
-    currency: '$',
-    label: 'Price',
-    options: {
-      attribute: 'unformated_price',
-    },
-  },
-  {
-    type: 'list',
-    header: 'Brand',
-    label: 'Brand',
-    options: {
-      attribute: 'brand',
-      searchable: true,
-    },
-  },
-  {
-    type: 'colour',
-    header: 'Colour',
-    label: 'Colour',
-    options: {
-      attribute: 'colour_hexa_v6',
-    },
-  },
-  {
-    type: 'list',
-    header: 'Gender',
-    label: 'Gender',
-    options: {
-      attribute: 'genderFilter',
-    },
-  },
-  {
-    type: 'size',
-    header: 'Sizes',
-    label: 'Size',
-    options: {
-      attribute: 'sizeFilter',
-      limit: 8,
-    },
-  },
-];
-
 const hitsPerPage = {
   numberNotInjected: 15,
   numberInjected: 14,
-};
-
-const refinementPrice = {
-  moreThan: 'More than',
-  lessThan: 'Less than',
 };
 
 // ------------------------------------------
@@ -160,10 +91,8 @@ export const hierarchicalFacet = {
 };
 
 const config = {
-  refinements,
   hitsPerPage,
   sortBy,
-  refinementPrice,
 };
 
 export const configAtom = atom({
