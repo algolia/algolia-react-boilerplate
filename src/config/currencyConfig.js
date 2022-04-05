3a77798b714db18e1006c49ce2e87d46d8dc4c10
Matt Foyle <mfoyle93@gmail.com>
2022-04-05 08:22:46 +0200

    fix: import atom for currency

--- src/config/currencyConfig.js
+++ src/config/currencyConfig.js
@@ -1,3 +1,5 @@
+import { atom } from 'recoil';
+
 export const currency = atom({
   key: 'currencyAtom', // unique ID (with respect to other atoms/selectors)
   default: '£', // default value (aka initial value)
