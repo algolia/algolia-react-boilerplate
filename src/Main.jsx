d529b92e2ac132c4df9363ffdb872e8e6f2e590b
Hugo Valla <Hugo_valla@hotmail.fr>
2022-04-05 11:14:00 +0200

    fea: Navigation panel for SE

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,19 +4,32 @@ import { AnimatePresence } from 'framer-motion';
 // React router
 import { Routes, Route, useLocation } from 'react-router-dom';
 
+//Import help navigation state & config
+import {
+  isHelpedNavigation,
+  shouldShowHelpedNavigation,
+} from './config/helpedNavigation';
+
 // Import Pages and static components
 import Header from './components/header/Header';
+import HelpNavigation from './components/helpNavigation/HelpNavigation';
 import HomePage from './pages/HomePage';
 import SearchResultsPage from './pages/SearchResultsPage';
 import ProductDetails from './pages/ProductDetails';
 import Footer from './components/footer/Footer';
+import { useRecoilValue } from 'recoil';
 
 export const Main = ({ isLoaded }) => {
   const location = useLocation();
+  const shouldShowNavigation = useRecoilValue(shouldShowHelpedNavigation);
+  const showHelpNavigation = useRecoilValue(isHelpedNavigation);
 
   return (
     <div className={`${isLoaded ? 'visible' : 'hidden'}`}>
       <Header />
+      <AnimatePresence>
+        {showHelpNavigation && shouldShowNavigation && <HelpNavigation />}
+      </AnimatePresence>
       <AnimatePresence initial={true} exitBeforeEnter>
         <Routes key={location.pathname} location={location}>
           <Route path="/" element={<HomePage />} />
