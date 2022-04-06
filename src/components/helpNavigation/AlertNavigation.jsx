3d3502cc3c556c97e8057b42f2df298bd940a7c1
Hugo Valla <Hugo_valla@hotmail.fr>
2022-04-06 10:08:38 +0200

    fix: Style of alert badge for demo guide

--- src/components/helpNavigation/AlertNavigation.jsx
+++ src/components/helpNavigation/AlertNavigation.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react';
 import { isAlertOpen, alertContent } from '@/config/helpedNavigation';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 const AlertNavigation = () => {
   const [alertOpen, setAlertOpen] = useRecoilState(isAlertOpen);
   const alertContentToDisplay = useRecoilValue(alertContent);
+
   return (
     <div className="wrap">
       {alertOpen && (
-        <div className="alert alert-info radius">
+        <div className="demoGuide__alert">
           <div>{alertContentToDisplay}</div>
-          <div className="cross" onClick={(e) => setAlertOpen(false)}>
+          <div
+            className="demoGuide__alert-close"
+            onClick={(e) => setAlertOpen(false)}
+          >
             X
           </div>
         </div>
