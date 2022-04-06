f8b2e87b2cf60b107cd480da8d305149c2a93f9b
VirAlex <alexou73@gmail.com>
2022-04-06 16:04:27 +0200

    fix: refacto custom hits unused code

--- src/components/hits/CustomHits.jsx
+++ src/components/hits/CustomHits.jsx
@@ -4,40 +4,17 @@
 import { motion } from 'framer-motion';
 
 // Import InstantSearch Functionality
-import { connectHits, Highlight } from 'react-instantsearch-dom';
+import { connectHits } from 'react-instantsearch-dom';
 
-// Import heart icon
-import { Heart } from '@/assets/svg/SvgIndex';
-import { framerMotionHits } from '@/config/animationConfig';
-
-// Recoil configuration
-import { hitAtom } from '@/config/hitsConfig';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { hitsConfig } from '@/config/hitsConfig';
-
-// React-router import
-import { useNavigate } from 'react-router-dom';
 import { Hit } from './Hits';
 
 const CustomHits = ({ hits }) => {
-  // Navigate is used by the router
-  const navigate = useNavigate();
-
-  // Get the global hit component
-  const hitState = useSetRecoilState(hitAtom);
-
-  // Get hit attribute from config file
-  const { price, objectID, image, category, productName } =
-    useRecoilValue(hitsConfig);
-
   return (
     <div className="ais-InfiniteHits">
       <ul className="ais-InfiniteHits-list">
         {hits.map((hit, i) => {
           // Wrap the hit info in an animation, and click functionality to view the product
-          return (
-              <Hit hit={hit} key={i} />
-          );
+          return <Hit hit={hit} key={i} />;
         })}
       </ul>
     </div>
