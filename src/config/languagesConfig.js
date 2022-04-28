6ade3f492c849973b2d113b6a7020d0dfb60bb55
Ben Daniel-Greep <bendanielgreep@gmail.com>
2022-04-28 11:00:50 +0100

    feature: port language select functionality from RL

--- src/config/languagesConfig.js
+++ src/config/languagesConfig.js
@@ -7,10 +7,6 @@ import { atom } from 'recoil';
 export const languagesConfig = [
   { value: 'English', label: 'English' },
   {
-    value: 'Spanish',
-    label: 'Spanish',
-  },
-  {
     value: 'German',
     label: 'German',
   },
@@ -32,19 +28,113 @@ export const languageSwitchConfig = {
   EN: {
     index: 'flagship_fashion',
     currency: '£',
+    linksHeader: [
+      {
+        name: 'All',
+        type: 'filter',
+        filter: '',
+        url: '/search',
+      },
+      {
+        name: 'Mens',
+        type: 'filter',
+        filter: 'Men',
+        url: '/mens',
+      },
+      {
+        name: 'Womens',
+        type: 'filter',
+        filter: 'Women',
+        url: '/womens',
+      },
+      {
+        name: 'Home',
+        type: 'filter',
+        filter: 'Home',
+        url: '/home',
+      },
+      {
+        name: 'Summer 22 Collection',
+        type: 'context',
+        context: 'Summer 22 Collection',
+        url: '/summer-22-collection',
+      },
+    ]
   },
   FR: {
     index: 'flagship_fashion_price_desc',
     currency: '€',
+    linksHeader: [
+      {
+        name: 'Tous',
+        type: 'filter',
+        filter: '',
+        url: '/search',
+      },
+      {
+        name: 'Hommes',
+        type: 'filter',
+        filter: 'Hommes',
+        url: '/hommes',
+      },
+      {
+        name: 'Femmes',
+        type: 'filter',
+        filter: 'Femmes',
+        url: '/femmes',
+      },
+      {
+        name: 'La Maison',
+        type: 'filter',
+        filter: 'La Maison',
+        url: '/la-maison',
+      },
+      {
+        name: 'Collection été 2022',
+        type: 'context',
+        context: 'Collection été 2022',
+        url: '/collection-ete-2022',
+      },
+    ]
+
   },
   GER: {
     index: 'flagship_fashion_ger',
     currency: '€',
+    linksHeader: [
+      {
+        name: 'All',
+        type: 'filter',
+        filter: '',
+        url: '/search',
+      },
+      {
+        name: 'Herren',
+        type: 'filter',
+        filter: 'Herren',
+        url: '/herren',
+      },
+      {
+        name: 'Damen',
+        type: 'filter',
+        filter: 'Damen',
+        url: '/damen',
+      },
+      {
+        name: 'Home',
+        type: 'filter',
+        filter: 'Home',
+        url: '/home',
+      },
+      {
+        name: 'Summer 22 Collection',
+        type: 'context',
+        context: 'Summer 22 Collection',
+        url: '/summer-22-collection',
+      },
+    ]
   },
-  SPA: {
-    index: 'flagship_fashion_spa',
-    currency: '€',
-  },
+  
 };
 
 // Styles for language selection dropdown, please ignore
