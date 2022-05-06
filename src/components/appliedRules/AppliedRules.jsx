16898aa5bf30388c544499cadd35e13451b1c052
Hugo Valla <Hugo_valla@hotmail.fr>
2022-05-06 14:15:11 +0200

    fix: rules applied

--- src/components/appliedRules/AppliedRules.jsx
+++ src/components/appliedRules/AppliedRules.jsx
@@ -5,17 +5,12 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 // Used for avoiding duplicates
 import { uniq } from 'lodash';
 
-import { queryAtom } from '@/config/searchboxConfig';
-
 // Config import
 import { mainIndex, searchClient } from '@/config/algoliaEnvConfig';
 import { rulesAtom } from '@/config/appliedRulesConfig';
 
 const AppliedRules = ({ searchResults }) => {
   const [rules, setRules] = useRecoilState(rulesAtom);
-
-  const query = useRecoilValue(queryAtom);
-
   // Init API request to get rules by their IDs
   const indexName = useRecoilValue(mainIndex);
   const index = searchClient.initIndex(indexName);
@@ -23,16 +18,19 @@ const AppliedRules = ({ searchResults }) => {
   // Get rules from Search result state
   // Store it in an array handled by recoil
   useEffect(() => {
-    setRules([]);
+    let rulesStorage = [];
     if (searchResults?.appliedRules !== null) {
-      let rules = searchResults?.appliedRules;
-      rules?.map((rule) => {
+      let rulesApplied = searchResults?.appliedRules;
+      rulesApplied?.map((rule) => {
         index.getRule(rule.objectID).then((e) => {
-          setRules((previousRules) => [...previousRules, e.description]);
+          rulesStorage.push(e.description);
+          let mergedRules = [...rulesStorage, e.description];
+          setRules(mergedRules);
+          // setRules((previousRules) => [...previousRules, e.description]);
         });
       });
     }
-  }, [searchResults.appliedRules]);
+  }, [searchResults.appliedRules, setRules]);
   // Create an array without duplicates
   const uniqRules = uniq(rules);
 
