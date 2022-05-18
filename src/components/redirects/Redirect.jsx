1f357b3a272eca6580e67f92225e1e078a773bbd
VirAlex <alexou73@gmail.com>
2022-05-18 10:52:42 +0200

    fix: redirect refacto thanks Hugo

--- src/components/redirects/Redirect.jsx
+++ src/components/redirects/Redirect.jsx
@@ -1,25 +1,11 @@
 import { useQueryRules } from 'react-instantsearch-hooks-web';
 
 function Redirect(props) {
-  // useQueryRules(props)
   const { items } = useQueryRules(props);
   const match = items.find((data) => Boolean(data.redirect));
   if (match && match.redirect) {
     window.location.href = match.redirect;
   }
-  //       window.location.href = match.redirect;
-  //     }
-  // useQueryRules({
-  //   transformItems(items) {
-  //     console.log('transformItems');
-  //     const match = items.find((data) => Boolean(data.redirect));
-  //     console.log(match);
-  //     if (match && match.redirect) {
-  //       window.location.href = match.redirect;
-  //     }
-  //     return [];
-  //   },
-  // });
   return null;
 }
 
