f37a9a690ccce46352076cd5d4a61a9ea8f822f1
Hugo Valla <Hugo_valla@hotmail.fr>
2022-05-18 10:46:57 +0200

    fix: redirect infinite loop

--- src/components/redirects/Redirect.jsx
+++ src/components/redirects/Redirect.jsx
@@ -1,17 +1,25 @@
 import { useQueryRules } from 'react-instantsearch-hooks-web';
 
-function Redirect() {
-  useQueryRules({
-    transformItems(items) {
-      console.log('transformItems');
-      const match = items.find((data) => Boolean(data.redirect));
-      console.log(match);
-      if (match && match.redirect) {
-        window.location.href = match.redirect;
-      }
-      return [];
-    },
-  });
+function Redirect(props) {
+  // useQueryRules(props)
+  const { items } = useQueryRules(props);
+  const match = items.find((data) => Boolean(data.redirect));
+  if (match && match.redirect) {
+    window.location.href = match.redirect;
+  }
+  //       window.location.href = match.redirect;
+  //     }
+  // useQueryRules({
+  //   transformItems(items) {
+  //     console.log('transformItems');
+  //     const match = items.find((data) => Boolean(data.redirect));
+  //     console.log(match);
+  //     if (match && match.redirect) {
+  //       window.location.href = match.redirect;
+  //     }
+  //     return [];
+  //   },
+  // });
   return null;
 }
 
