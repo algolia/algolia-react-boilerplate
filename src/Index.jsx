bd41e7d15736ca57d8b35ef77673f7a032317b16
Matt Foyle <mfoyle93@gmail.com>
2022-06-17 09:19:26 +0200

    feat: wip SSR

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// React Tour
 import { TourProvider } from '@reactour/tour';
+import ReactDOM from 'react-dom';
+import viteSSR from 'vite-ssr';
 
 import App from './App';
 
-import { steps } from '@/config/demoTourConfig'
+import { steps } from '@/config/demoTourConfig';
 
-ReactDOM.render(
-  // React Tour
-  <TourProvider steps={steps} showBadge={false}>
-    <App />
-  </TourProvider>,
-  document.querySelector('#root')
-);
+const Main = () => {
+  return ReactDOM.render(
+    // React Tour
+    <TourProvider steps={steps} showBadge={false}>
+      <App />
+    </TourProvider>,
+    document.querySelector('#root')
+  );
+};
+
+export default viteSSR(Main, ({ url, isClient, request }) => {
+  // Custom initialization hook
+});
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import path from 'path';
 
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import viteSSR from 'vite-ssr/plugin.js';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteSSR(), react(), viteCompression()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-helmet-async'],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
