ceed5aff84c3812f24a375c2b684b1576fc46f50
Matthew Foyle <mfoyle93@gmail.com>
2022-06-23 10:58:35 +0200

    hotfix: turn off demo tour

--- src/config/demoTourConfig.js
+++ src/config/demoTourConfig.js
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 // Should we show the demo tour in this demo
 export const shouldShowDemoTour = atom({
   key: 'shouldShowDemoTour',
-  default: true,
+  default: false,
 });
 
 // Steps for the demo tour, these are adjustable
