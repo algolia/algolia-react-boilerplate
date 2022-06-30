7f56df94690b166329b385c28ff62e4440de20e7
VirAlex <alexou73@gmail.com>
2022-06-30 17:43:50 +0200

    fix: remove console.log

--- src/components/redirects/Redirect.jsx
+++ src/components/redirects/Redirect.jsx
@@ -2,7 +2,6 @@ import { useQueryRules } from 'react-instantsearch-hooks-web';
 
 function Redirect(props) {
   const { items } = useQueryRules(props);
-  console.log(items);
   const match = items.find((data) => Boolean(data.redirect));
   if (match && match.redirect) {
     window.location.href = match.redirect;
