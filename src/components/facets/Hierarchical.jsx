071f36c9b72ee8954fb91537c428b2a4aefbafd0
VirAlex <alexou73@gmail.com>
2022-06-30 10:15:19 +0200

    fix: add info

--- src/components/facets/Hierarchical.jsx
+++ src/components/facets/Hierarchical.jsx
@@ -3,28 +3,63 @@ import { useHierarchicalMenu } from 'react-instantsearch-hooks-web';
 
 // This component is recursive, to allow subcategories to be displayed
 // eg. Categories > Mens > Clothing > Jackets
-const CustomHierarchicalMenu = (props) => {
-  const { items, refine } = useHierarchicalMenu(props);
-  const { title } = props;
-  // console.log('Je Passe la');
-  // console.log(props);
-  // console.log(
-  //   '🚀 ~ file: Hierarchical.jsx ~ line 8 ~ CustomHierarchicalMenu ~ items',
-  //   items
-  // );
-  // const { itemsData } = props;
-  // console.log(
-  //   '🚀 ~ file: Hierarchical.jsx ~ line 9 ~ CustomHierarchicalMenu ~ itemsData',
-  //   itemsData
-  // );
+function CustomHierarchicalMenu(props) {
+  const { items, refine, createURL, onNavigate } = useHierarchicalMenu(props);
+  const { title, itemsData } = props;
 
-  return (
+  return itemsData ? (
     <div className="filters-container-hierarchical">
       <div className="filters-container-hierarchical__title">
         <h3>{title}</h3>
       </div>
       <ul className="filters-container-hierarchical__content">
         {items.map((item) => {
+          return (
+            <li
+              className="filters-container-hierarchical__content__list"
+              key={item.value}
+            >
+              <a
+                className={`filters-container-hierarchical__content__list__button-filter ${
+                  item.isRefined ? 'refined-filter' : ''
+                }`}
+                type="button"
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  refine(item.value);
+                }}
+              >
+                <p>{item.label}</p>
+                <span className="filters-container-hierarchical__content__list__refinement-count">
+                  {item.count}
+                </span>
+              </a>
+              {/* If there are items within the 'data' object, then display them */}
+              {item?.data && (
+                // <div className="filters-container-hierarchical__content__list-isOpened">
+                <CustomHierarchicalMenu
+                  className="filters-container-hierarchical__content__list-isOpened"
+                  items={item.data}
+                  refine={refine}
+                  onNavigate={onNavigate}
+                  createURL={createURL}
+                />
+                // </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="filters-container-hierarchical">
+      <div className="filters-container-hierarchical__title">
+        <h3>{title}</h3>
+      </div>
+      <ul className="filters-container-hierarchical__content">
+        {items.map((item) => {
+          console.log(item.data);
           return (
             <li
               className="filters-container-hierarchical__content__list"
@@ -46,10 +81,13 @@ const CustomHierarchicalMenu = (props) => {
                   {item.count}
                 </span>
               </button>
-              {/* If there are items within the 'item' object, then display them */}
+              {/* If there are items within the 'data' object, then display them */}
               {item.data && (
                 <div className="filters-container-hierarchical__content__list-isOpened">
-                  <CustomHierarchicalMenu items={item.data} refine={refine} />
+                  <CustomHierarchicalMenu
+                    itemsData={item.data}
+                    refine={refine}
+                  />
                 </div>
               )}
             </li>
@@ -58,6 +96,6 @@ const CustomHierarchicalMenu = (props) => {
       </ul>
     </div>
   );
-};
+}
 
 export default CustomHierarchicalMenu;
