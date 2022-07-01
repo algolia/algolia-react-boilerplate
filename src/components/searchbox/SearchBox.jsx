6f4e70090f8c549be2630c48c3faa144df43006e
VirAlex <alexou73@gmail.com>
2022-07-01 16:46:18 +0200

    fix: onClick qs merging

--- src/components/searchbox/SearchBox.jsx
+++ src/components/searchbox/SearchBox.jsx
@@ -1,7 +1,7 @@
 // This SearchBox is with a magnifying glass inside
 // but simple it means with only a glass simple effect
 
-import { memo, useState } from 'react';
+import { memo, useState, useEffect } from 'react';
 
 // Algolia Import
 import { useSearchBox } from 'react-instantsearch-hooks-web';
