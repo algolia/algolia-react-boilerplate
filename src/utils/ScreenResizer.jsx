71cbcf51289da00de3b28abafda85d63863d75f0
Hugo Valla <Hugo_valla@hotmail.fr>
2022-09-07 16:33:17 +0200

    header navigation responsive for size

--- src/utils/ScreenResizer.jsx
+++ src/utils/ScreenResizer.jsx
@@ -4,34 +4,33 @@ import { useEffect } from 'react';
 import { windowSize } from '@/hooks/useScreenSize';
 
 const ScreenResizer = () => {
+  const setWindowSize = useSetRecoilState(windowSize);
 
-    const setWindowSize = useSetRecoilState(windowSize);
+  const handleResize = () => {
+    // Set screen size and return true or false
+    const view = Math.min(screen.width, window.innerWidth);
 
-    const handleResize = () => {
-      // Set screen size and return true or false
-      const view = Math.min(screen.width, window.innerWidth)
+    setWindowSize({
+      laptop: view >= 1440,
+      laptopXS: view > 820 && view < 1440,
+      tablet: view >= 480 && view <= 820,
+      mobile: view < 480,
+      isDesktop: view >= 1440 || (view > 820 && view < 1440),
+    });
+  };
 
-      setWindowSize({
-        laptop: view >= 1440,
-        laptopXS: view > 820 && view < 1440,
-        tablet: view >= 480 && view <= 820,
-        mobile: view < 480,
-        isDesktop: (view >= 1440) || (view > 768 && view < 1440)
-      });
-    };
-  
-    useEffect(() => {
-      // Add event listener
-      window.addEventListener('resize', handleResize);
-  
-      // Call handler right away so state gets updated with initial window size
-      handleResize();
-      
-      // Remove event listener on cleanup
-      return () => window.removeEventListener('resize', handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
+  useEffect(() => {
+    // Add event listener
+    window.addEventListener('resize', handleResize);
 
-    return <></>
-}
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return <></>;
+};
 
 export default ScreenResizer;
