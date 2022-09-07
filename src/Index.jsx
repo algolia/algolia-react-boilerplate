28f354fd10d93fca3fecfda93b34d89187df85db
Matthew Foyle <mfoyle93@gmail.com>
2022-09-07 14:02:31 +0200

    fix: remove tour for now

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,13 +1,10 @@
-import { TourProvider } from '@reactour/tour';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { steps } from '@/config/demoTourConfig';
+
 
 // Translations for the app and links,....
 import '@/config/translation';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TourProvider steps={steps} showBadge={false}>
     <App />
-  </TourProvider>
 );
