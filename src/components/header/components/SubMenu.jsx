6b5756b0aae61b48de07402638a49014e6cc67a8
VirAlex <alexou73@gmail.com>
2022-09-15 09:45:23 +0200

    fix: home image

--- src/components/header/components/SubMenu.jsx
+++ src/components/header/components/SubMenu.jsx
@@ -150,6 +150,12 @@ const SubMenu = ({ categoryName, setDisplaySubMenu }) => {
           alt=""
         />
       )}
+      {categoryName.name === 'Home' && (
+        <img
+          src="https://play-lh.googleusercontent.com/rHIlOS9pb_qQ6H8DRmw6vzlth2CTWlXIpEuWa9nch-zhWn7zOE5-5RNZSlwfsGtlRiRF"
+          alt=""
+        />
+      )}
     </div>
   );
 };
