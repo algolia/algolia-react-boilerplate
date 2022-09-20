dde2ec721275edd9e403cfafe693685eddcd74bd
Hugo Valla <Hugo_valla@hotmail.fr>
2022-09-20 15:04:34 +0200

    remove fontawesome & lazy load react router pages

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,11 +1,19 @@
+import { AnimatePresence, motion } from 'framer-motion';
 // import SCSS
 import './SCSS/loader.scss';
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <div className="loader__in"></div>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="loader"
+      >
+        <div className="loader__in"></div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
