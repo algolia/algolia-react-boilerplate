99c26d1b0c21167de15ab5c0400a2569a6123830
VirAlex <alexou73@gmail.com>
2022-09-23 11:08:48 +0200

    fix: started modal

--- src/components/cart/ArticlesCard.jsx
+++ src/components/cart/ArticlesCard.jsx
@@ -1,7 +1,26 @@
-const ArticlesCard = () => {
-  return (
-    <div>ArticlesCard</div>
-  )
-}
+// Recoil import
+import { hitAtom, hitsConfig } from '@/config/hitsConfig';
+import get from 'lodash/get';
 
-export default ArticlesCard
+const ArticlesCard = ({ item }) => {
+  // Get hit attribute from config file
+  const {
+    objectID,
+    image,
+    imageAlt,
+    category,
+    productName,
+    brand,
+    price: priceForTotal,
+  } = hitsConfig;
+  return (
+    <div className="articles-card">
+      <img src={get(item, image)} alt="" />
+      <div className='articles-card__infos'>
+        <h3>Test</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ArticlesCard;
--- src/components/cart/Modal.jsx
+++ src/components/cart/Modal.jsx
@@ -1,9 +1,25 @@
 import ArticlesCard from './ArticlesCard';
 
+import { cartState } from '@/config/cartFunctions';
+import { useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
+
+//Import scope SCSS
+import './SCSS/cartModal.scss';
+
 const Modal = () => {
+  const cartValue = useRecoilValue(cartState);
+  useEffect(() => {
+    console.log(cartValue);
+  }, [cartValue]);
+
   return (
-    <div>
-      <ArticlesCard />
+    <div className="modal-container">
+      <h3 className="modal-container__title">My Cart(3)</h3>
+      <div className="modal-container__line"></div>
+      {cartValue.map((item) => {
+        return <ArticlesCard item={item} />;
+      })}
     </div>
   );
 };
