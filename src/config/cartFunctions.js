12053c67021de845af66eab0dbc50c37ce8724f8
VirAlex <alexou73@gmail.com>
2022-09-30 11:39:52 +0200

    feat add in selectors

--- src/config/cartFunctions.js
+++ src/config/cartFunctions.js
@@ -59,7 +59,6 @@ export const addToCartSelector = selector({
       // Iterate on our cart
       cart.map((item, index) => {
         if (item.objectID === newProduct.objectID) {
-          console.log('first');
           // And
           // If we already have the same article have
           // we store the index of this on cartItemIndex
@@ -75,62 +74,16 @@ export const addToCartSelector = selector({
         };
         console.log(items);
         // Store in the cart the new array Items
-        set(productQtyState[cartItemIndex].qty);
-        set(cartState, [items]);
+        // set(productQtyState[cartItemIndex].qty);
+        set(cartState, items);
+      } else {
+        // If not already the same article
+        console.log('first');
+        set(cartState, [
+          ...cart,
+          { ...newProduct, qty: 1, totalPrice: newProduct[hitsConfig.price] },
+        ]);
       }
     }
   },
 });
-
-// export const addToCartSelector = selector({
-//   key: 'addToCartSelector',
-// get: ({ get }) => get(order),
-// set: ({ set, get }, newArticle) => {
-//   set(order, [...get(order), newArticle]);
-//   if (cart.length < 1) {
-// Store it
-// set(cartState, [{ ...product, qty: 1, totalPrice: product[priceForTotal] }]);
-// setProductQty(1);
-//   }
-// },
-// set: ({set}, newValue) => set(myAtom, newValue instanceof DefaultValue ? newValue : newValue / 100),
-// });
-
-// const addToCart = (product, productQty) => {
-//   // If we have not already a Cart
-//   if (cart.length < 1) {
-//     // Store it
-//     setCart([{ ...product, qty: 1, totalPrice: product[priceForTotal] }]);
-//     setProductQty(1);
-//   } else {
-//     // Define a null const
-//     let cartItemIndex = null;
-//     // Iterate on our cart
-//     cart.map((item, index) => {
-//       if (item.objectID === product.objectID) {
-//         // And
-//         // If we already have the same article have
-//         // we store the index of this on cartItemIndex
-//         cartItemIndex = index;
-//       }
-//     });
-//     if (cartItemIndex !== null) {
-//       let items = [...cart];
-//       items[cartItemIndex] = {
-//         ...items[cartItemIndex],
-//         qty: productQty + 1,
-//         totalPrice: (productQty + 1) * items[cartItemIndex][priceForTotal],
-//       };
-//       // Store in the cart the new array Items
-//       setProductQty(items[cartItemIndex].qty);
-//       setCart(items);
-//     } else {
-//       // If not already the same article
-//       setCart([
-//         ...cart,
-//         { ...product, qty: 1, totalPrice: product[priceForTotal] },
-//       ]);
-//       setProductQty(1);
-//     }
-//   }
-// };
