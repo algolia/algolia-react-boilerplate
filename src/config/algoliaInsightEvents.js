aa02b505de6b59debf523e2c189fcc05f9801154
Hugo Valla <Hugo_valla@hotmail.fr>
2022-10-06 10:29:29 +0200

    event working need to replicate

--- src/config/algoliaInsightEvents.js
+++ src/config/algoliaInsightEvents.js
@@ -1,20 +1,27 @@
+import { atom, selector, useRecoilValue } from 'recoil';
+import { personaSelectedAtom } from './personaConfig';
+
 import aa from 'search-insights';
 import { createInsightsMiddleware } from 'instantsearch.js/es/middlewares';
 import { useLayoutEffect } from 'react';
 import { useInstantSearch } from 'react-instantsearch-hooks-web';
 
-
+// const personaSelected = selector({
+//   key: 'PersonaSelected',
+//   get: ({get}) => get(personaSelectedAtom)
+// });
 
 export function InsightsMiddleware() {
   const { use } = useInstantSearch();
-
+  const user = useRecoilValue(personaSelectedAtom)
+  
   useLayoutEffect(() => {
-   
+    
     const middleware = createInsightsMiddleware({
       insightsClient: aa,
     });
 
-    const userToken = null; // Get the user token (synchronously or asynchronously).
+    const userToken = user === 'anonymous-user' ? 'anonymous-user' : user ; // Get the user token (synchronously or asynchronously).
     // The `insights` middleware receives a notification
     // and attaches the `userToken` to search calls onwards.
     aa('setUserToken', userToken);
