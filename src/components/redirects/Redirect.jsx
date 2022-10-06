12d40a9005363d098ba8ce6f4e2b45817a19dbbc
Hugo Valla <Hugo_valla@hotmail.fr>
2022-10-06 18:05:59 +0200

    redirect modal done for mobile

--- src/components/redirects/Redirect.jsx
+++ src/components/redirects/Redirect.jsx
@@ -1,14 +1,46 @@
-import { divide } from 'lodash';
+//Algolia's import
 import { useQueryRules } from 'react-instantsearch-hooks-web';
-import { useNavigate } from 'react-router-dom';
+
+// Import Recoil
+import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+
+// Import React router
+import { useSearchParams } from 'react-router-dom';
+
+// Import Config
+import { showRedirectModal } from '@/config/redirectConfig';
+import { queryAtom } from '@/config/searchboxConfig';
+
+import { windowSize } from '@/hooks/useScreenSize';
+
+//importing CSS
+import './SCSS/redirect.scss';
 
 function Redirect(props) {
   const { items } = useQueryRules(props);
+  const [isRedirectModal, setIsRedirectModal] =
+    useRecoilState(showRedirectModal);
+
+  const setQuery = useSetRecoilState(queryAtom);
+
+  const { isDesktop } = useRecoilValue(windowSize);
+
+  // Handle URL search parameters through React Router
+  let [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams);
 
   const match = items.find((data) => Boolean(data.redirect));
-  if (match && match.redirect) {
+  match && match.redirect
+    ? setIsRedirectModal(true)
+    : setIsRedirectModal(false);
+
+  if (match && match.redirect && isRedirectModal) {
     return (
-      <div className="redirectModal">
+      <div
+        className={
+          isDesktop ? 'redirectModal' : 'redirectModal redirectModal-mobile'
+        }
+      >
         <div className="redirectModal__infos">
           <p>This query will take you to Alolia's Homepage</p>
           <p>Do you wish to be redirected ? </p>
@@ -17,11 +49,23 @@ function Redirect(props) {
           <a
             href="#"
             className="redirectModal__buttons-ok"
-            onClick={() => (window.location.href = match.redirect)}
+            onClick={() => {
+              window.location.href = match.redirect;
+              setIsRedirectModal(false);
+            }}
           >
             <p>Yes I want to be redirected</p>
           </a>
-          <a href="#" className="redirectModal__buttons-no">
+          <a
+            href="#"
+            className="redirectModal__buttons-no"
+            onClick={() => {
+              setQuery('');
+              searchParams.set('query', '');
+              setSearchParams(searchParams);
+              setIsRedirectModal(false);
+            }}
+          >
             <p>No I want to stay on this demo</p>
           </a>
         </div>
