40a0728dc3d62e4092d80f10cae01d60a3dd87b2
Diego Larraz <diegolarraz1@gmail.com>
2022-10-13 15:03:06 +0100

    test

--- src/components/searchresultpage/SearchResults/SearchResults.jsx
+++ src/components/searchresultpage/SearchResults/SearchResults.jsx
@@ -224,7 +224,7 @@ const SearchResults = () => {
               navigationState?.type === 'context' ? navigationState.action : ''
             }
             query={
-              finalQuery
+              queryState
             }
             getRankingInfo={true}
           />
