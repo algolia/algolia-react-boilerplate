0025f047c769033fdeb06b328e20f2332e3300ec
Matthew Foyle <mfoyle93@gmail.com>
2022-10-19 21:21:39 +0200

    chore: add lint and prettier config together

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,8 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-
-  // extends: [
-  //   'algolia',
-  //   'algolia/react',
-  //   'plugin:react/recommended',
-  //   'plugin:react/jsx-runtime',
-  // ],
+  extends: [
+    "plugin:prettier/recommended"
+  ],
 
   parser: '@babel/eslint-parser',
   rules: {
@@ -32,4 +28,4 @@ module.exports = {
       version: 'detect', // you have to tell eslint which version React are using, "detect" will say can you just figure out yourself (from package.json)
     },
   },
-};
+}
