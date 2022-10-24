349c8c670ab23f7e143fdfa990c5aae56733ebcb
Matthew Foyle <mfoyle93@gmail.com>
2022-10-24 09:22:16 +0200

    hotfix: remove react-refresh

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import inject from '@rollup/plugin-inject'
 
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       plugins: [
