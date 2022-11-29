e4e06b1769aa62c421205e4ced474cc3aab5a8cd
Ben Daniel-Greep <bendanielgreep@gmail.com>
2022-11-29 10:26:20 +0000

    edit config url to match netlify

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Boilerplate Documentation',
   tagline: 'Egg',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://monumental-melba-89d587.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
