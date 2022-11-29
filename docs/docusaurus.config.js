d09857911751052ae1213a4bac0a2511257d175d
Ben Daniel-Greep <bendanielgreep@gmail.com>
2022-11-29 10:53:02 +0000

    fox: rm blog reference from footer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -107,10 +107,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
