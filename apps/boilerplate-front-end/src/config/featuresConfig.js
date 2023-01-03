736c9d1699c36131fa6f2580c2f60d2a0fa466e7
Hugo Valla <Hugo_valla@hotmail.fr>
2023-01-03 11:49:48 +0100

    passing features to true

--- apps/boilerplate-front-end/src/config/featuresConfig.js
+++ apps/boilerplate-front-end/src/config/featuresConfig.js
@@ -17,20 +17,20 @@ export const shouldHaveQRCode = atom({
 // Demo guide is here to help you demo your POC. Before turning the feature ON you can configure it in demoGuideConfig.js
 export const shouldHaveDemoGuide = atom({
   key: 'shouldHaveDemoGuide',
-  default: false,
+  default: true,
 })
 
 // Should the segment selector be displayed on the screen
 export const shouldHaveSegments = atom({
   key: 'shouldHaveSegments', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 // Make sure to go into languageConfig.js to set everything up
 // Should the language selector be displayed on the screen
 export const shouldHaveLanguages = atom({
   key: 'shouldHaveLanguages', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 // Make sure to create replicas in the dashboard
@@ -57,21 +57,21 @@ export const shouldHaveStats = atom({
 // Make sure you create a rule in the dahsboard before turn it on
 export const shouldHaveInjectedBanners = atom({
   key: 'shouldHaveInjectedBanners',
-  default: false,
+  default: true,
 })
 
 // Make sure your sending event and create userToken before turn it on
 // You can set up personas details in personaConfig.js
 export const shouldHavePersona = atom({
   key: 'shouldHavePersona',
-  default: false,
+  default: true,
 })
 
 // Make sure you create the injected content in rules or through another index before turning this on, to avoid errors
 // Please see https://github.com/algolia/algolia-react-boilerplate#--injected-content for more info on injected content
 export const shouldHaveInjectedHits = atom({
   key: 'shouldHaveInjectedHits',
-  default: false,
+  default: true,
 })
 
 export const shouldHaveFederatedSearch = atom({
@@ -83,14 +83,14 @@ export const shouldHaveFederatedSearch = atom({
 // Make sure you trained the models into the dashboards first
 export const shouldHaveRelatedProducts = atom({
   key: 'shouldHaveRelatedProductsAtom', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 // RECOMMEND
 // Make sure you trained the models into the dashboards first
 export const shouldHaveFbtProducts = atom({
   key: 'shouldHaveFbtProducts', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 // RECOMMEND
@@ -98,7 +98,7 @@ export const shouldHaveFbtProducts = atom({
 // this feature will be visible on home, results and category pages if activated -> (activated by default)
 export const shouldHaveTrendingProducts = atom({
   key: 'shouldHaveTrendingProductsAtom', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 // RECOMMEND
@@ -106,18 +106,18 @@ export const shouldHaveTrendingProducts = atom({
 // this feature will be visible on home, results and category pages if activated -> (activated by default)
 export const shouldHaveTrendingFacets = atom({
   key: 'shouldHaveTrendingFacetsAtom', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 export const shouldHaveDynamicFacet = atom({
   key: 'shouldHaveDynamicFacet', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 // Make sure to create a rule in the dashboard to make it work
 export const shouldHaveRedirect = atom({
   key: 'shouldHaveRedirect', // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: true, // default value (aka initial value)
 })
 
 export const shouldHaveCartFunctionality = atom({
