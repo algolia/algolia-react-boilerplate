5ecf1ed9c4f1bf515a1d964d493888bd41b145a2
Hugo Valla <Hugo_valla@hotmail.fr>
2023-03-31 17:32:06 +0200

    fix: query was not cleared or refresh when navigating from SRP to Homepage

--- apps/boilerplate-front-end/src/components/searchbox/SearchBox.jsx
+++ apps/boilerplate-front-end/src/components/searchbox/SearchBox.jsx
@@ -70,8 +70,8 @@ function CustomSearchBox({ refine, query, clear }) {
   }, [query])
 
   const searchOnChangeHandler = (event) => {
-    // setLocalQuery(event.target.value)
     refine(event.target.value)
+
     // debouncedSearchInputHandler(event.target.value)
     setTooltip(false)
   }
@@ -138,7 +138,7 @@ function CustomSearchBox({ refine, query, clear }) {
           onChange={(event) => {
             // refine(event.target.value)
             searchOnChangeHandler(event)
-            setTimeout(() => handleContextInUrl(event), 1000)
+            // setTimeout(() => handleContextInUrl(event), 1000)
           }}
         />
         {!!tooltip && (
